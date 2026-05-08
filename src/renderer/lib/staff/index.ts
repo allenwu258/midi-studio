2a2f0c1a49ad4f8e82b8de33ed0fbdccf026fee5
RichardMilos <2500455688@qq.com>
2026-05-08 10:38:14 +0800

    Add staff score SVG export helper

--- src/renderer/lib/staff/index.ts
+++ src/renderer/lib/staff/index.ts
@@ -1,6 +1,7 @@
 export { layoutScore } from "./layout";
 export { beamCount } from "./beams";
 export { DEFAULT_RENDER_LAYOUT_OPTIONS } from "./types";
+export { renderScoreToSvg } from "./svgExport";
 export type {
   RenderBeamGroup,
   RenderBeamPoint,
